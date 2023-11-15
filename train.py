a818ed319e5ea84e8e0bb8dabe9b6959919a3833
OlineRanum <oline.a.ranum@gmail.com>
2023-11-15 18:21:53 +0100

    Update environment name

--- train.py
+++ train.py
@@ -2,7 +2,7 @@ import omegaconf
 from openhands.apis.classification_model import ClassificationModel
 from openhands.core.exp_utils import get_trainer
 
-cfg = omegaconf.OmegaConf.load("configs/gcn_adapter_train.yaml")
+cfg = omegaconf.OmegaConf.load("configs/gcn_base_all_train.yaml")
 trainer = get_trainer(cfg)
 
 model = ClassificationModel(cfg=cfg, trainer=trainer)
